2966fdfe32bc59218ac741526aa2e31fd97699d7
Pepa <bukovskyjosef@gmail.com>
2026-04-17 12:01:15 +0200

    hra-rozšíření o všechny písně

--- game/app.js
+++ game/app.js
@@ -1,13 +1,6 @@
 (function () {
-  // IDs for which a real image file exists in pics/
-  const HAS_IMAGE = new Set([
-    1,2,3,4,5,6,7,8,9,
-    11,12,13,14,15,16,17,
-    19,20,21,22,23,24,25,26,
-    28,29,30,31,32,33,34,35,
-    38,39,40,41,42,43,
-    47,49,51,52,55,58,
-  ]);
+  // All 78 songs now have images
+  const HAS_IMAGE = new Set(Array.from({ length: 78 }, (_, i) => i + 1));
 
   const ALL_ITEMS = [
     { id: 1,  label: 'Výletnická I.' },
