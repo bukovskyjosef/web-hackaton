a715d3b65e0b18df67b845467af771890f10f549
Pepa <bukovskyjosef@gmail.com>
2026-09-14 21:45:30 +0200

    novy termin koncertu

--- assets/app.js
+++ assets/app.js
@@ -10,9 +10,6 @@ const WIDGETS = [
   'widgets/widget-organizatori.html',
 ];
 const CONCERTS = [
-  'concerts/koncert-5.html',
-  'concerts/koncert-6.html',
-  'concerts/koncert-7.html',
   'concerts/koncert-8.html',
 ];
 const MEMBERS = [
